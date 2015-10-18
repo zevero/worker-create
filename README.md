f7dc3ce66ba012048f1fbcb4e52253039be97508
Puschkarski Severin <severin@puschkarski.org>
2015-10-18 13:58:52 +0200

    Allow create from string

--- README.md
+++ README.md
@@ -3,10 +3,13 @@ Create a worker without using a seperate worker.js file
 
 ###Preperation (just once):
  
-    var worker_url = Worker.create(function(e){
+    var worker_url = Worker.create(function(e){  //from function
       self.postMessage('Example post from Worker'); //your code here
     });
 
+or
+
+    var worker_url = Worker.create("self.postMessage('Example post from Worker');"); //from string
 
 ###Usage:
 
--- worker-create.js
+++ worker-create.js
@@ -2,19 +2,22 @@
 
 /* https://github.com/zevero/worker-create 
  *
- * Create a worker without using a seperate worker.js file
+ * Create a worker without using a seperate worker.js file either from function or from string
  * 
  * Preparation:
- * var worker_url = Worker.create(function(e){ //only once
+ * var worker_url = Worker.create(function(e){ //from function
  *   self.postMessage('Example post from Worker'); //your code here
  * });
+ * or
+ * var worker_url = Worker.create("self.postMessage('Example post from Worker');"); //from string
  *
  * Usage:
  * var worker = new Worker(worker_url); //as often as you like
  * 
  */
 'use strict';
-Worker.create = function(worker_fn){
-    var blob = new Blob(['\'use strict\';\nself.onmessage ='+worker_fn.toString()], { type: 'text/javascript' });
+Worker.create = function(func_or_string){
+    var str = (typeof func_or_string === 'function')?func_or_string.toString():func_or_string;
+    var blob = new Blob(['\'use strict\';\nself.onmessage ='+str], { type: 'text/javascript' });
     return window.URL.createObjectURL(blob);
 };
